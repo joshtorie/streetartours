6c1b52596b480bc50cc4854d13cc8c25e85647ff
joshtorie <yitzmeyer@gmail.com>
2024-12-21 01:46:54 +0200

    supabase

---
+++ src/supabaseClient.ts
@@ -0,0 +1,8 @@
+// src/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
+
+// Replace these with your Supabase project URL and public API key
+const supabaseUrl = https://bckufhwabrscilsqcwnb.supabase.co;
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJja3VmaHdhYnJzY2lsc3Fjd25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3Mzc5MDksImV4cCI6MjA1MDMxMzkwOX0.lNDNd2V5pNLBojave3jSUkK2OII9Vl2cq-ZfZi0XKvw;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
