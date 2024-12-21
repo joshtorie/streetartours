9417f326618822761c49db7c5b5008ec0cd008df
joshtorie <yitzmeyer@gmail.com>
2024-12-21 02:09:10 +0200

    env

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,6 @@
-// src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-// Replace these with your Supabase project URL and public API key
-const supabaseUrl = 'https://bckufhwabrscilsqcwnb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJja3VmaHdhYnJzY2lsc3Fjd25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3Mzc5MDksImV4cCI6MjA1MDMxMzkwOX0.lNDNd2V5pNLBojave3jSUkK2OII9Vl2cq-ZfZi0XKvw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
